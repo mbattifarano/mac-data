9396918d8d769bc2f715910f4e967c379bb6a397
Matt Battifarano <matthew.battifarano@gmail.com>
2018-03-11 21:04:57 -0400

    Fix tests

--- tests/test_mac_data.py
+++ tests/test_mac_data.py
@@ -31,7 +31,7 @@ def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'Command line collection of mobility data' in result.output
+    assert 'Command line tool for data collection from web APIs' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
-    assert '--help     Show this message and exit.' in help_result.output
+    assert 'Show this message and exit.' in help_result.output
